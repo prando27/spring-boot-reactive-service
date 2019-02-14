4d5be9d22300c99bb021aafc53e41c8940396812
José Prando <jose.prando@ifood.com.br>
2019-02-14 00:21:11 -0200

    docs

--- src/main/java/com/prando/springbootreactiveservice/SpringBootFullReactiveServiceApplication.java
+++ src/main/java/com/prando/springbootreactiveservice/SpringBootFullReactiveServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cache.annotation.EnableCaching;
 /**
  * 1 - REST using functional endpoints - Ok
  * 2 - Cache - OK (In memory cache) - Ok
+ *  2.1 - https://stackoverflow.com/questions/48156424/spring-webflux-and-cacheable-proper-way-of-caching-result-of-mono-flux-type
  * 3 - Persistence using R2DBC and Postgres - Almost there...
  */
 @SpringBootApplication
