ba8edd1ebceb63b999eb154c315b759e1e2e30e7
José Prando <jose.prando@ifood.com.br>
2019-02-17 12:09:27 -0300

    redis-template-option

--- src/main/java/com/prando/springbootreactiveservice/component/PersonService.java
+++ src/main/java/com/prando/springbootreactiveservice/component/PersonService.java
@@ -2,41 +2,42 @@ package com.prando.springbootreactiveservice.component;
 
 import com.prando.springbootreactiveservice.model.Person;
 import io.micrometer.core.instrument.util.StringUtils;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
+import reactor.cache.CacheMono;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Signal;
 
 import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 @Service
 public class PersonService {
 
     private final PersonRepository personRepository;
-//    private final ReactiveRedisTemplate<String, Person> reactiveRedisTemplate;
+    private final ReactiveRedisTemplate<String, Person> reactiveRedisTemplate;
 
     public PersonService(PersonRepository personRepository,
-//                         ReactiveRedisTemplate<String, Person> reactiveRedisTemplate) {
-    ) {
+                         ReactiveRedisTemplate<String, Person> reactiveRedisTemplate) {
         this.personRepository = personRepository;
-//        this.reactiveRedisTemplate = reactiveRedisTemplate;
+        this.reactiveRedisTemplate = reactiveRedisTemplate;
     }
 
     public Mono<Person> findPersonById(Integer id) {
-        return Mono.empty();
-//        return CacheMono.lookup(findPersonReader(), id)
-//                .onCacheMissResume(personRepository.findById(id))
-//                .andWriteWith(findPersonWriter());
+        return CacheMono.lookup(findPersonReader(), id)
+                .onCacheMissResume(personRepository.findById(id))
+                .andWriteWith(findPersonWriter());
     }
 
-//    private Function<Integer, Mono<Signal<? extends Person>>> findPersonReader() {
-//        return key -> Mono.justOrEmpty(cacheManager.getCache("person").get(key, Person.class)).map(Signal::next);
-//        return key -> reactiveRedisTemplate.opsForValue().get(key).map(Signal::next);
-//    }
+    private Function<Integer, Mono<Signal<? extends Person>>> findPersonReader() {
+        return key -> reactiveRedisTemplate.opsForValue().get(key).map(Signal::next);
+    }
 
-//    private BiFunction<Integer, Signal<? extends Person>, Mono<Void>> findPersonWriter() {
-//        return (key, value) -> Mono.fromRunnable(() -> Optional.ofNullable(value.get()).ifPresent(val -> cacheManager.getCache("person").put(key, val)));
-//        return (key, value) -> reactiveRedisTemplate.opsForValue().set(key.toString(), value.get()).then();
-//    }
+    private BiFunction<Integer, Signal<? extends Person>, Mono<Void>> findPersonWriter() {
+        return (key, value) -> reactiveRedisTemplate.opsForValue().set(key.toString(), value.get()).then();
+    }
 
     public Flux<Person> listPerson() {
         return personRepository.findAll();
@@ -49,10 +50,10 @@ public class PersonService {
     public Mono<Person> updatePerson(Integer id, Person person) {
         return personRepository.findById(id)
                 .map(personFound -> this.patch(personFound, person))
-                .flatMap(personToUpdate -> personRepository.save(personToUpdate))
-//                        .flatMap(personUpdated -> reactiveRedisTemplate.opsForValue().set(id.toString(), personUpdated)
-//                                .then(Mono.just(personUpdated))
-//                                .onErrorReturn(personUpdated)))
+                .flatMap(personToUpdate -> personRepository.save(personToUpdate)
+                        .flatMap(personUpdated -> reactiveRedisTemplate.opsForValue().set(id.toString(), personUpdated)
+                                .then(Mono.just(personUpdated))
+                                .onErrorReturn(personUpdated)))
                 .switchIfEmpty(Mono.empty());
     }
 
@@ -65,7 +66,7 @@ public class PersonService {
 
     public Mono<Void> removePerson(Integer id) {
         return personRepository.deleteById(id)
-//                .then(reactiveRedisTemplate.opsForValue().delete(id.toString()))
+                .then(reactiveRedisTemplate.opsForValue().delete(id.toString()))
                 .then();
     }
 }
--- src/main/java/com/prando/springbootreactiveservice/config/RedisConfig.java
+++ src/main/java/com/prando/springbootreactiveservice/config/RedisConfig.java
@@ -1,7 +1,22 @@
 package com.prando.springbootreactiveservice.config;
 
+import io.lettuce.core.resource.ClientResources;
+import io.lettuce.core.resource.DefaultClientResources;
+import io.lettuce.core.resource.Delay;
+import io.lettuce.core.resource.DirContextDnsResolver;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -15,57 +30,31 @@ public class RedisConfig {
     @Value("${redis.command-timeout}")
     private int commandTimeout;
 
-//    @Bean
-//    public RedisClient redisClient() {
-//        ClientResources clientResources = DefaultClientResources.builder()
-//                .dnsResolver(new DirContextDnsResolver())
-//                .reconnectDelay(Delay.constant(Duration.ofSeconds(reconnectDelay)))
-//                .build();
-//
-//        return RedisClient.create(clientResources);
-//    }
-//
-//    @Bean
-//    public StatefulRedisConnection<String, String> statefulRedisConnection(RedisClient redisClient) {
-//        RedisURI redisURI = RedisURI.create(host, port);
-//
-//        StatefulRedisConnection<String, String> statefulRedisConnection = redisClient.connect(redisURI);
-//        statefulRedisConnection.setTimeout(Duration.ofSeconds(commandTimeout));
-//
-//        return statefulRedisConnection;
-//    }
-//
-//    @Bean
-//    public RedisReactiveCommands<String, String> redisCommands(StatefulRedisConnection<String, String> statefulRedisConnection) {
-//        return statefulRedisConnection.reactive();
-//    }
+    @Bean
+    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
+        ClientResources clientResources = DefaultClientResources.builder()
+                .dnsResolver(new DirContextDnsResolver())
+                .reconnectDelay(Delay.constant(Duration.ofSeconds(reconnectDelay)))
+                .build();
 
+        LettuceClientConfiguration lettuceClientConfiguration = LettuceClientConfiguration.builder()
+                .clientResources(clientResources)
+                .commandTimeout(Duration.ofSeconds(commandTimeout))
+                .build();
 
-//    @Bean
-//    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
-//        ClientResources clientResources = DefaultClientResources.builder()
-//                .dnsResolver(new DirContextDnsResolver())
-//                .reconnectDelay(Delay.constant(Duration.ofSeconds(reconnectDelay)))
-//                .build();
-//
-//        LettuceClientConfiguration lettuceClientConfiguration = LettuceClientConfiguration.builder()
-//                .clientResources(clientResources)
-//                .commandTimeout(Duration.ofSeconds(commandTimeout))
-//                .build();
-//
-//        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port), lettuceClientConfiguration);
-//    }
-//
-//    @Bean
-//    public ReactiveRedisTemplate<String, Object> reactiveRedisTemplate(ReactiveRedisConnectionFactory reactiveRedisConnectionFactory) {
-//
-//        RedisSerializationContext.RedisSerializationContextBuilder<String, Object> builder =
-//                RedisSerializationContext.newSerializationContext(new StringRedisSerializer());
-//
-//        RedisSerializationContext<String, Object> redisSerializationContext = builder
-//                .value(new GenericJackson2JsonRedisSerializer("_type")).build();
-//
-//        return new ReactiveRedisTemplate<>(reactiveRedisConnectionFactory, redisSerializationContext);
-//    }
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port), lettuceClientConfiguration);
+    }
+
+    @Bean
+    public ReactiveRedisTemplate<String, Object> reactiveRedisTemplate(ReactiveRedisConnectionFactory reactiveRedisConnectionFactory) {
+
+        RedisSerializationContext.RedisSerializationContextBuilder<String, Object> builder =
+                RedisSerializationContext.newSerializationContext(new StringRedisSerializer());
+
+        RedisSerializationContext<String, Object> redisSerializationContext = builder
+                .value(new GenericJackson2JsonRedisSerializer("_type")).build();
+
+        return new ReactiveRedisTemplate<>(reactiveRedisConnectionFactory, redisSerializationContext);
+    }
 
 }
